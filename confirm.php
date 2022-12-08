c770c63f82cd07348c51dc37f09eeda59e2e1e37
Guilherme-Vilatoro <120067203+Guilherme-Vilatoro@users.noreply.github.com>
2022-12-07 19:35:45 -0500

    Update confirm.php

--- confirm.php
+++ confirm.php
@@ -12,12 +12,12 @@
 		echo "<p>".$_POST['address']."</p>";
 		echo "<p>".$_POST['CCN']."</p>";
 		echo "<p>".$_POST['date']."</p>";
-		echo "<p>".$_POST['CVV']."</p>";
+		echo "<p>".$_POST['CCV']."</p>";
 		
 		$cnx = new mysqli('localhost','root','pass','database');
 		if($cnx->connect_error)
 			die('Connection failed: '.$cnx->connect_error);
-		$query = 'insert into client ( first_name, last_name, address, ccn, exp_date, cvv ) values ( "'.$_POST['name'].'","'.$_POST['lname'].'","'.$_POST['address'].'","'.$_POST['CCN'].'","'.$_POST['date'].'",'.$_POST['CVV'].')';
+		$query = 'insert into client ( first_name, last_name, address, ccn, exp_date, cvv ) values ( "'.$_POST['name'].'","'.$_POST['lname'].'","'.$_POST['address'].'","'.$_POST['CCN'].'","'.$_POST['date'].'",'.$_POST['CCV'].')';
 		echo"<p>".$query."</p>";
 		$cursor = $cnx->query($query);
 		$cnx->close();
